88976adbbdd18702ac67b5b4cfd6da81d3ab29b9
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2023-12-27 16:31:42 +0530

    Concurrent sum array

---
+++ src/main/java/org/example/concurrencyPPA/concurrentSumArray/Worker.java
@@ -0,0 +1,30 @@
+package org.example.concurrencyPPA.concurrentSumArray;
+
+import java.util.List;
+
+public class Worker implements Runnable{
+
+    private final List<Integer> nums;
+
+    private final int start,end;
+
+    public Worker(List<Integer> nums, int start, int end){
+
+        this.nums = nums;
+        this.start = start;
+        this.end = end;
+    }
+
+
+    @Override
+    public void run(){
+        long local_sum=0;
+        for(int i=start;i<=end;i++){
+            local_sum+=nums.get(i);
+        }
+
+        Main.sum.addAndGet(local_sum);
+        Main.cnt.incrementAndGet();
+    }
+
+}
